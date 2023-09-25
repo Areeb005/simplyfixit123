a9f91ee2ca23b5bf796eea70f13086f7f4cc8154
MustafaAlvi21 <mustafaalvi21@gmail.com>
2023-09-25 19:00:22 +0500

    'update'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,20 @@ function Footer() {
         <>
             <footer className="footer ">
                 <div className="container">
-                    <div className="d-flex justify-content-center py-5">
+                    <div className="as00 py-5">
+                        <p className='mb-0'>
+                            <span style={{ color: "white" }}>⛏ </span>
+                            Build By
+                            <a href="https://dtecherz.com"
+                            target='_blank'
+                                style={{
+                                    textDecoration: "none",
+                                    color: "var(--theme-yellow-color)",
+                                    fontWeight: 600
+                                }}>
+                                &nbsp;dTecherz
+                            </a>
+                        </p>
                         <p className='mb-0'>© 2023 Company, Inc. All rights reserved.</p>
                     </div>
                 </div>
